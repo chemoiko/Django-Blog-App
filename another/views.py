961d6eaf9a22dbae9d26a2d6bb4a3d8af9ecbaf1
ochom <ochom@police.com>
2024-06-08 13:12:56 +0300

    4-html templates

---
+++ another/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.contrib import admin
+from django.shortcuts import render     #this is to render stuff
+
+
+
+
+def homepage(request):
+    return render(request, 'homepage.html')
+
+def about(request):
+    return render(request, 'about.html')
