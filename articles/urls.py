3658e502649a2bfc321d3bc31d59a43d978cd721
elijah <elijahmurket@gmail.com>
2024-06-10 15:38:36 +0300

    15  named urls

--- articles/urls.py
+++ articles/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 
-
+app_name = 'articles'   #this is called namespacing to not confuse the urls of multiple apps e.g name = 'detail' could also be in another app called office so it would finally be called articles:detail
 urlpatterns = [
    
     path('', views.article_list),
-    path('<slug:abc>/', views.article_detail, name='article_detail'),
+    path('<slug:abc>/', views.article_detail, name='detail'),
 ]
