983c2facdd070bef6617d5b8abbc54bdd66953ff
elijah <elijahmurket@gmail.com>
2024-06-12 20:40:17 +0300

    26  model-forms

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Article
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from . import forms
 
 # Create your views here.
 def article_list(request):
@@ -18,4 +19,13 @@ def article_detail(request, abc):
 
 @login_required(login_url="/accounts/login/")       #this is a decorator protecting this view from anyone who hasnt logged in and anyone who is not logged in will be redirected to that url
 def article_create(request):
-    return render(request, 'articles/article_create.html')    #send it back to a template
+    if request.method =='POST':
+        form = forms.CreateArticle(request.POST, request.FILES)
+        if form.is_valid():
+            #save article to db
+            return redirect('articles:list')
+    else:
+        form = forms.CreateArticle()
+    return render(request, 'articles/article_create.html', {'form':form})    #send it back to a template
+
+
